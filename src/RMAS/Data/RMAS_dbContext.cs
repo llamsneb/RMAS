29d3ac587f6526bf7f0e95812d0a9cc37ecce62e
Ben Small <llamsneb@gmail.com>
2023-11-05 17:00:07 -0600

    Migration update models changes.

--- src/RMAS/Data/RMAS_dbContext.cs
+++ src/RMAS/Data/RMAS_dbContext.cs
@@ -14,161 +14,110 @@ namespace RMAS.Models
         {
             modelBuilder.Entity<AspNetRoleClaims>(entity =>
             {
-                entity.HasIndex(e => e.RoleId)
-                    .HasName("IX_AspNetRoleClaims_RoleId");
+                entity.HasIndex(e => e.RoleId, "IX_AspNetRoleClaims_RoleId");
 
-                entity.Property(e => e.RoleId)
-                    .IsRequired()
-                    .HasMaxLength(450);
+                entity.Property(e => e.RoleId).IsRequired();
 
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.AspNetRoleClaims)
-                    .HasForeignKey(d => d.RoleId);
+                entity.HasOne(d => d.Role).WithMany(p => p.AspNetRoleClaims).HasForeignKey(d => d.RoleId);
             });
 
             modelBuilder.Entity<AspNetRoles>(entity =>
             {
-                entity.HasIndex(e => e.NormalizedName)
-                    .HasName("RoleNameIndex");
-
-                entity.Property(e => e.Id).HasMaxLength(450);
+                entity.HasIndex(e => e.NormalizedName, "RoleNameIndex");
 
                 entity.Property(e => e.Name).HasMaxLength(256);
-
                 entity.Property(e => e.NormalizedName).HasMaxLength(256);
             });
 
             modelBuilder.Entity<AspNetUserClaims>(entity =>
             {
-                entity.HasIndex(e => e.UserId)
-                    .HasName("IX_AspNetUserClaims_UserId");
+                entity.HasIndex(e => e.UserId, "IX_AspNetUserClaims_UserId");
 
-                entity.Property(e => e.UserId)
-                    .IsRequired()
-                    .HasMaxLength(450);
+                entity.Property(e => e.UserId).IsRequired();
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserClaims)
-                    .HasForeignKey(d => d.UserId);
+                entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims).HasForeignKey(d => d.UserId);
             });
 
             modelBuilder.Entity<AspNetUserLogins>(entity =>
             {
-                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey })
-                    .HasName("PK_AspNetUserLogins");
+                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
 
-                entity.HasIndex(e => e.UserId)
-                    .HasName("IX_AspNetUserLogins_UserId");
+                entity.HasIndex(e => e.UserId, "IX_AspNetUserLogins_UserId");
 
-                entity.Property(e => e.LoginProvider).HasMaxLength(450);
+                entity.Property(e => e.UserId).IsRequired();
 
-                entity.Property(e => e.ProviderKey).HasMaxLength(450);
-
-                entity.Property(e => e.UserId)
-                    .IsRequired()
-                    .HasMaxLength(450);
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserLogins)
-                    .HasForeignKey(d => d.UserId);
+                entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins).HasForeignKey(d => d.UserId);
             });
 
             modelBuilder.Entity<AspNetUserRoles>(entity =>
             {
-                entity.HasKey(e => new { e.UserId, e.RoleId })
-                    .HasName("PK_AspNetUserRoles");
+                entity.HasKey(e => new { e.UserId, e.RoleId });
 
-                entity.HasIndex(e => e.RoleId)
-                    .HasName("IX_AspNetUserRoles_RoleId");
+                entity.HasIndex(e => e.RoleId, "IX_AspNetUserRoles_RoleId");
 
-                entity.HasIndex(e => e.UserId)
-                    .HasName("IX_AspNetUserRoles_UserId");
+                entity.HasIndex(e => e.UserId, "IX_AspNetUserRoles_UserId");
 
-                entity.Property(e => e.UserId).HasMaxLength(450);
+                entity.HasOne(d => d.Role).WithMany(p => p.AspNetUserRoles).HasForeignKey(d => d.RoleId);
 
-                entity.Property(e => e.RoleId).HasMaxLength(450);
-
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.AspNetUserRoles)
-                    .HasForeignKey(d => d.RoleId);
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserRoles)
-                    .HasForeignKey(d => d.UserId);
+                entity.HasOne(d => d.User).WithMany(p => p.AspNetUserRoles).HasForeignKey(d => d.UserId);
             });
 
             modelBuilder.Entity<AspNetUserTokens>(entity =>
             {
-                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name })
-                    .HasName("PK_AspNetUserTokens");
-
-                entity.Property(e => e.UserId).HasMaxLength(450);
-
-                entity.Property(e => e.LoginProvider).HasMaxLength(450);
-
-                entity.Property(e => e.Name).HasMaxLength(450);
+                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
             });
 
             modelBuilder.Entity<AspNetUsers>(entity =>
             {
-                entity.HasIndex(e => e.NormalizedEmail)
-                    .HasName("EmailIndex");
+                entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
 
-                entity.HasIndex(e => e.NormalizedUserName)
-                    .HasName("UserNameIndex")
-                    .IsUnique();
-
-                entity.Property(e => e.Id).HasMaxLength(450);
+                entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedUserName] IS NOT NULL)");
 
                 entity.Property(e => e.Email).HasMaxLength(256);
-
                 entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
-
-                entity.Property(e => e.NormalizedUserName)
-                    .IsRequired()
-                    .HasMaxLength(256);
-
+                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
                 entity.Property(e => e.UserName).HasMaxLength(256);
             });
 
             modelBuilder.Entity<Event>(entity =>
             {
-                entity.HasKey(e => new { e.EventDate, e.BeginTime, e.EndTime, e.RoomNumber })
-                    .HasName("PK__Event__A5B70DB060BCA16D");
+                entity.HasKey(e => new { e.EventDate, e.BeginTime, e.EndTime, e.RoomNumber }).HasName("PK__Event__A5B70DB0772F9B4E");
 
                 entity.Property(e => e.EventDate).HasColumnType("date");
-
                 entity.Property(e => e.EventName)
                     .IsRequired()
-                    .HasColumnType("varchar(45)");
-
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
                 entity.Property(e => e.UserId)
                     .IsRequired()
                     .HasMaxLength(450);
 
-                entity.HasOne(d => d.RoomNumberNavigation)
-                    .WithMany(p => p.Event)
+                entity.HasOne(d => d.RoomNumberNavigation).WithMany(p => p.Event)
                     .HasForeignKey(d => d.RoomNumber)
                     .HasConstraintName("RoomNumber");
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Event)
+                entity.HasOne(d => d.User).WithMany(p => p.Event)
                     .HasForeignKey(d => d.UserId)
                     .HasConstraintName("UserId");
             });
 
             modelBuilder.Entity<Room>(entity =>
             {
-                entity.HasKey(e => e.RoomNumber)
-                    .HasName("PK__Room__AE10E07B26BAF33B");
+                entity.HasKey(e => e.RoomNumber).HasName("PK__Room__AE10E07B7E0C1FAE");
 
                 entity.Property(e => e.RoomNumber).ValueGeneratedNever();
-
                 entity.Property(e => e.RoomType)
                     .IsRequired()
-                    .HasColumnType("varchar(45)");
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
             });
+
+            OnModelCreatingPartial(modelBuilder);
         }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
         public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
         public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
